bd70869769f600bb83b5a14e178c8e05b0d851ea
ikanago <28985004+ikanago@users.noreply.github.com>
2020-07-18 14:37:03 +0900

    ✨ Build a simple page with webpack

---
+++ front/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.tsx',
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.tsx', '.js', '.jsx',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/template/index.html' })],
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        port: 5000,
+        open: true
+    },
+};
