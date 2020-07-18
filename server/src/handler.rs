5dd7a3b977aa88122569a83a519aac8fd618054c
ikanago <28985004+ikanago@users.noreply.github.com>
2020-07-18 21:08:22 +0900

    ✨ Implement stub handler

---
+++ server/src/handler.rs
@@ -0,0 +1,22 @@
+use actix_web::{web, HttpResponse, HttpRequest};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Register {
+    text: String,
+}
+
+pub async fn hello() -> String {
+    "hello\n".to_string()
+}
+
+/// Handle incoming requests with user's post.
+pub async fn handle_receive_post(form: web::Form<Register>) -> HttpResponse {
+    dbg!(form);
+    HttpResponse::Ok().finish()
+}
+
+/// Handle incoming requests to get current posts.
+pub async fn handle_get_posts(req: HttpRequest) -> HttpResponse {
+    unimplemented!();
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use std::io;
+
+mod handler;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/submit", web::post().to(handler::handle_receive_post))
+            .route("/", web::get().to(handler::hello))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
